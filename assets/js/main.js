abe242c8a80a582b71c02dda16c6f28e81bd4c2a
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-09-22 14:38:14 +0000

    added contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "PI38ULXyHLhiD3skz",
   });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
+})()
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // these IDs from the previous steps
+    emailjs.sendForm('service_uqdxjtr', 'contact_form', this)
+      .then(() => {
+        console.log('SUCCESS!');
+      }, (error) => {
+        console.log('FAILED...', error);
+      });
   });
-
-  (function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init({
-      publicKey: "PI38ULXyHLhiD3skz",
-    });
-})();
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // these IDs from the previous steps
-        emailjs.sendForm('service_uqdxjtr', 'contact_form', this)
-            .then(() => {
-                console.log('SUCCESS!');
-            }, (error) => {
-                console.log('FAILED...', error);
-            });
-    });
 }
