9346c0d62e40d420cc82b459e81ae7c0c313cf64
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-10-03 12:52:12 +0000

    changed email

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,24 @@ document.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function () {
   $('.sidenav').sidenav();
 });
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "PI38ULXyHLhiD3skz",
+  });
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm('service_uqdxjtr', 'contact_form', this)
+          .then(() => {
+              console.log('SUCCESS!');
+          }, (error) => {
+              console.log('FAILED...', error);
+          });
+  });
+}
+
 
